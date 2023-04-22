a4df15144c762e6469a59eca0a087492a320bd45
adampalecki <adampalecki97@gmail.com>
2023-04-22 14:37:17 +0200

    x

---
+++ data.js
@@ -0,0 +1,57 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2023-04-02-at-193853_stitch",
+      "name": "Zdjęcie 1 - wejście do zakrystii",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1590.5,
+      "initialViewParameters": {
+        "yaw": 1.3771968482256973,
+        "pitch": -0.07996876146318321,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.840811624936645,
+          "pitch": 0.006476284474311811,
+          "rotation": 0,
+          "target": "0-whatsapp-image-2023-04-02-at-193853_stitch"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3233056620937749,
+          "pitch": -0.137090669226545,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Czujnik 1</font></font>",
+          "text": "<div><a href='https://www.mapa.network/d/5LJp1xHVki/displacement?orgId=11&from=1665935912256&to=1681660712256&viewPanel=14'>Czujnik zarysowania</a> i <a href='https://www.mapa.network/d/HWDGybHVz/temperatury-i-wilgotnosci?orgId=11&from=1665935946589&to=1681660746589&viewPanel=6'>czujnik temperatury i wilgotności wewnętrznej</a></div>"
+
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
